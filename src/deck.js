2be90ee55b01e427b05ddf02c31a0eb64b6df789
theory friction practice <jackson.brebnor@gmail.com>
2017-03-07 08:34:47 -0500

    deck cleanup

--- src/deck.js
+++ src/deck.js
@@ -1,8 +1,3 @@
 import { Deck, } from 'bee52/';
 
 export const { deck, shuffle, } = Deck;
-
-// console.log(Deck.deck());
-// export const bdex = Deck.deck();
-// export const deck = Deck.de
-// export const discard = (deck)=>
