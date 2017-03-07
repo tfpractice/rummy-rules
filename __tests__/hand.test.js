b6c38a71871d04e510f5e5dcc167e8220df0c20a
theory friction practice <jackson.brebnor@gmail.com>
2017-03-07 12:52:18 -0500

    ads cardsmodule

--- __tests__/hand.test.js
+++ __tests__/hand.test.js
@@ -1,6 +1,6 @@
 import 'jasmine-expect';
 import { Deck, } from 'bee52';
-import { hand, } from 'src/hand';
+import { hand, handGraph, } from 'src/hand';
 
 const { shuffle, deck, } = Deck;
 const myDeck = shuffle(deck());
@@ -9,6 +9,12 @@ describe('hand', () => {
   describe('hand', () => {
     it('retrieves 7 cards from a deck', () => {
       expect(hand(myDeck).length).toEqual(7);
+    });
+  });
+  describe('handGraph', () => {
+    it('retuns a map of cards ', () => {
+      console.log(handGraph(...hand(myDeck)));
+      expect(handGraph(...hand(myDeck)) instanceof Map).toBeTrue();
     });
   });
 });
--- src/hand.js
+++ src/hand.js
@@ -1,7 +1,10 @@
 import { Deck, } from 'bee52';
+import { Graph, } from 'graph-curry';
 
+const { graph, nodes, } = Graph;
 const { draw, } = Deck;
 
 export const hand = deck => draw(7)(deck);
+export const handGraph = graph;
 
 // export const discard = card=> deck=> draw(1)()
