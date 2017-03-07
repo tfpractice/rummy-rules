17a5c22f46a747884dacecbd12d6ec990074ddf4
theory friction practice <jackson.brebnor@gmail.com>
2017-03-07 13:20:14 -0500

    compare

--- __tests__/cards/compare.test.js
+++ __tests__/cards/compare.test.js
@@ -1,6 +1,7 @@
 import 'jasmine-expect';
 import { card,copy,hasRank,hasSuit, } from 'src/cards/card';
-import { rankDiff, } from 'src/cards/compare';
+import { rankDiff,rankOrder, } from 'src/cards/compare';
+
 const d2 = card('2','DIAMONDS');
 const d3 = card('3','DIAMONDS');
 const c2 = card('2','CLUBS');
@@ -8,9 +9,14 @@ const cA = card('a','CLUBS');
 const myCards = [ d2,d3,c2,cA, ];
 
 describe('compare', () => {
-  describe('rankDiff', () => {
+  describe('rankOrder', () => {
     it('returns the difference in rankVal of two cards', () => {
-      expect(rankDiff(d2, d3)).toEqual(-1);
+      expect(rankOrder(d2, d3)).toEqual(-1);
+    });
+  });
+  describe('rankDiff', () => {
+    it('returns the rank difference', () => {
+      expect(rankOrder(d2)(d3)).toEqual(-1);
     });
   });
 });
--- src/cards/compare.js
+++ src/cards/compare.js
@@ -6,10 +6,11 @@ const { rank, } = Card;
 
 // const { nextRank,rankVal, } = Rank;
 
-export const rankDiff = (a,b) => rankVal(rank(a)) - rankVal(rank(b));
+export const rankDiff = a => b => rankVal(rank(a)) - rankVal(rank(b));
+export const rankOrder = (a,b) => rankVal(rank(a)) - rankVal(rank(b));
 export const rankSort = cards => cards.sort(rankDiff);
 
 // export const nextBin = (cards,b) => hasNext(b)(cards);
-export const isAdj = c0 => c1 => Math.abs(rankDiff(c0,c1)) === 1;
+export const isAdj = c0 => c1 => Math.abs(rankDiff(c0)(c1)) === 1;
 
 // export const sequence = cards => c => cards.filter(isAdj(c)).concat(c).sort(rankDiff);
