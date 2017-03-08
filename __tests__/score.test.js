578523b04fc8551ef7f7c9e58c5999045df3a4a8
theory friction practice <jackson.brebnor@gmail.com>
2017-03-08 16:10:39 -0500

    scores

---
+++ __tests__/score.test.js
@@ -0,0 +1,2 @@
+  import * from 'jasmine-expect';
+  import { scoreMap } from 'src/score';
---
+++ src/score.js
@@ -0,0 +1,3 @@
+import { RANKS, } from 'bee52';
+
+export const scoreMap = (rank, index) => index > 10 ? 5 : 10;
