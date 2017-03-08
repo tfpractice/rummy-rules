ae465f77c9f99c89520c58f461affc6cb42b434c
theory friction practice <jackson.brebnor@gmail.com>
2017-03-08 14:26:09 -0500

    diffSuit and isEquiv

--- __tests__/cards/compare.test.js
+++ __tests__/cards/compare.test.js
@@ -1,6 +1,6 @@
 import 'jasmine-expect';
 import { card,copy,hasRank,hasSuit, } from 'src/cards/card';
-import { isAdj,rankAdj, rankDiff,rankOrder, } from 'src/cards/compare';
+import { diffSuit,isAdj, isEquiv,rankAdj,rankDiff, rankOrder, } from 'src/cards/compare';
 
 const d2 = card('2','DIAMONDS');
 const d3 = card('3','DIAMONDS');
@@ -23,13 +23,25 @@ describe('compare', () => {
     it('checks if two cards have a rankDiff of 1', () => {
       expect(rankAdj(d2)(d3)).toBeTrue();
       expect(rankAdj(c2)(d3)).toBeTrue();
-      expect(rankAdj(c2)(c2)).toBeTrue();
+      expect(rankAdj(c2)(c2)).toBeFalse();
     });
   });
   describe('isAdj', () => {
     it('checks if two cards are off the same suit and adjacent buy rank', () => {
       expect(isAdj(d2)(d3)).toBeTrue();
       expect(isAdj(c2)(d3)).toBeFalse();
+    });
+  });
+  describe('diffSuit', () => {
+    it('checks for nonequality between suits', () => {
+      expect(diffSuit(d2)(c2)).toBeTrue();
+      expect(diffSuit(cA)(c2)).toBeFalse();
+    });
+  });
+  describe('isEquiv', () => {
+    it('checks for suit and rank equality', () => {
+      expect(isEquiv(c2)(c2)).toBeTrue();
+      expect(isEquiv(c2)(d2)).toBeFalse();
     });
   });
 });
--- src/cards/compare.js
+++ src/cards/compare.js
@@ -13,3 +13,4 @@ export const rankSort = cards => cards.sort(rankDiff);
 export const rankAdj = c0 => c1 => adjRanks(c0).map(hasRank).some(f => f(c1));
 export const isAdj = c0 => c1 => [ sameSuit(c0), rankAdj(c0), ].every(f => f(c1));
 export const suitAdj = c0 => c1 => [ diffSuit(c0), sameRank(c0), ].every(f => f(c1));
+export const isEquiv = c0 => c1 => [ sameRank(c0), sameSuit(c0), ].every(f => f(c1));
