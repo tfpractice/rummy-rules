a91f7820bc6e20d3a8c4d8e0b7f7d26320f3f4c2
theory friction practice <jackson.brebnor@gmail.com>
2017-03-08 19:25:28 -0500

    points and penalty

--- __tests__/score.test.js
+++ __tests__/score.test.js
@@ -1,2 +1,67 @@
-  import * from 'jasmine-expect';
-  import { scoreMap } from 'src/score';
+import 'jasmine-expect';
+import { allAces, penalty, points, score, scoreSet, seqScore, sumBin, total, } from 'src/score';
+import { deck, } from 'src/deck';
+import { sequences, } from 'src/cards/join';
+const myDeck = (deck());
+const s26 = sequences(myDeck.slice(0, 27));
+
+describe('score', () => {
+  describe('points', () => {
+    describe('when the suit is less than 10', () => {
+      it('returns 5', () => {
+        expect(points('2')).toEqual(5);
+      });
+    });
+    describe('when the suit is greater than 10', () => {
+      it('returns 10', () => {
+        expect(points('a')).toEqual(10);
+      });
+    });
+  });
+  describe('penalty', () => {
+    describe('when the rank is not an ace', () => {
+      it('returns the negated value of the rank', () => {
+        expect(penalty('2')).toEqual(-5);
+      });
+    });
+    describe('when the rank is an ace', () => {
+      it('returns negative 15', () => {
+        expect(penalty('a')).toEqual(-15);
+      });
+    });
+  });
+  describe('sumBin', () => {
+    it('adds the rankScore to the sum', () => {
+      expect(sumBin(10, 'a')).toEqual(20);  
+    });
+  });
+  describe('seqScore', () => {
+    it('scores a sequence of cards', () => {
+      expect(seqScore(myDeck)).toEqual(360);
+    });
+  });
+  describe('allAces', () => {
+    it('checks if every cards is an ace', () => {
+      expect(allAces(myDeck.filter(c => c.rank === 'a'))).toBeTruthy();
+    });
+  });
+  describe('scoreSet', () => {
+    it('scores a set of cards', () => {
+      expect(scoreSet(myDeck.splice(0, 5))).toEqual(25);
+      expect(scoreSet(myDeck.filter(c => c.rank === 'a'))).toEqual(60);
+    });
+  });
+  describe('total', () => {
+    it('sums the sets of hands', () => {
+      expect(total(s26)).toBe(185);
+    });
+  });
+  describe('penalty', () => {
+    it('returns a negated score for cards left in hand', () => {
+      expect(penalty('2')).toEqual(-5);
+      
+      expect(penalty('10')).toEqual(-10);
+      expect(penalty('a')).toEqual(-15);
+    });
+  });
+});
--- src/score.js
+++ src/score.js
@@ -1,3 +1,21 @@
-import { RANKS, } from 'bee52';
+import { Rank, } from 'bee52';
+import { spread, } from 'fenugreek-collections';
+import { hasRank, rank, } from './cards/card';
+const { rankVal, } = Rank;
 
-export const scoreMap = (rank, index) => index > 10 ? 5 : 10;
+export const allAces = cards => spread(cards).every(hasRank('a'));
+
+export const score = r => rankVal(r) < 8 ? 5 : 10;
+export const points = r => rankVal(r) < 8 ? 5 : 10;
+export const penalty = r => r === 'a' ? -15 : -1 * score(r);
+export const sumBin2 = (sum = 0, x) => sum + x;
+
+export const sumBin = (sum = 0, r) => sum + score(r);
+export const seqScore = cards => spread(cards).map(rank).map(score).reduce(sumBin2,0);
+export const aceScore = aces => spread(aces).length * 15;
+
+export const scoreSet = cards => allAces(cards) ? aceScore(cards) : seqScore(cards);
+export const scoreSetBin = (total = 0, set) => total + scoreSet(set);
+export const total = sets => spread(sets).reduce(scoreSetBin,0);
+
+export const deductions = inHand => inHand.map(rank).map(penalty).reduce(sumBin2,0);
