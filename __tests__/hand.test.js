c8b77a9186099a35f0bf296adc71c4cdc0ef6d9e
theory friction practice <jackson.brebnor@gmail.com>
2017-03-08 21:22:50 -0500

    possibles?

--- __tests__/hand.test.js
+++ __tests__/hand.test.js
@@ -3,7 +3,9 @@ import { Deck, } from 'bee52';
 import { hand,rankPlays, rankPoss,seqPlays, seqPoss, } from 'src/hand';
 import { sequences, } from 'src/cards/join';
 const { shuffle, deck, } = Deck;
-const myDeck = shuffle(deck());
+const myDeck = (deck());
+const myHand = myDeck.slice(1, 7);
+const first = myDeck[0];
 
 describe('hand', () => {
   describe('hand', () => {
@@ -23,6 +25,7 @@ describe('hand', () => {
   
   describe('seqPlays', () => {
     it('returns the sets containing the poitential card', () => {
+      console.log(seqPoss(first)(myHand));
       expect(seqPlays(myDeck)).toBeArray();
     });
   }); describe('rankPlays', () => {
