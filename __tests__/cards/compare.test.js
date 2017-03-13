32076fa9d44dd9ec8f56dabf4e7dcaf7299ee983
theory friction practice <jackson.brebnor@gmail.com>
2017-03-12 21:19:54 -0400

    adjacency tests. compare refactored

--- __tests__/cards/compare.test.js
+++ __tests__/cards/compare.test.js
@@ -1,6 +1,6 @@
 import 'jasmine-expect';
 import { card, } from 'src/cards/card';
-import { diffSuit,isAdj, isMatch,rankAdj, rankDiff,rankOrder,rankSort,xMatch, } from 'src/cards/compare';
+import { isAdj,rankAdj, suitAdj, } from 'src/cards/compare';
 
 const d2 = card('2','DIAMONDS');
 const d3 = card('3','DIAMONDS');
@@ -10,16 +10,6 @@ const cA = card('a','CLUBS');
 const myCards = [ d2,d3,c2,cA, ];
 
 describe('compare', () => {
-  describe('rankOrder', () => {
-    it('returns the difference in rankVal of two cards', () => {
-      expect(rankOrder(d2, d3)).toEqual(-1);
-    });
-  });
-  describe('rankDiff', () => {
-    it('returns the rank difference', () => {
-      expect(rankDiff(d2)(d3)).toEqual(-1);
-    });
-  });
   describe('rankAdj', () => {
     it('checks if two cards have a rankDiff of 1', () => {
       expect(rankAdj(d2)(d3)).toBeTrue();
@@ -31,30 +21,6 @@ describe('compare', () => {
     it('checks if two cards are off the same suit of adjacent rank', () => {
       expect(isAdj(d2)(d3)).toBeTrue();
       expect(isAdj(c2)(d3)).toBeFalse();
-    });
-  });
-  describe('diffSuit', () => {
-    it('checks for nonequality between suits', () => {
-      expect(diffSuit(d2)(c2)).toBeTrue();
-      expect(diffSuit(cA)(c2)).toBeFalse();
-    });
-  });
-  describe('isMatch', () => {
-    it('checks for suit and rank equality', () => {
-      expect(isMatch(c2)(c2)).toBeTrue();
-      expect(isMatch(c2)(d2)).toBeFalse();
-    });
-  }); 
-  describe('xMatch', () => {
-    it('checks for suit and rank equality', () => {
-      expect(xMatch(c2)(c2)).toBeFalse();
-      expect(xMatch(c2)(d2)).toBeTrue();
-    });
-  });
-  describe('rankSort', () => {
-    it('sorts an array by increasin rank', () => {
-      console.log(rankSort(myCards));
-      expect(rankSort(myCards)).toBeArray();
     });
   });
 });
--- src/cards/compare.js
+++ src/cards/compare.js
@@ -1,18 +1,8 @@
-import { Card,Rank, } from 'bee52';
-import { adjRanks,hasRank, sameRank, sameSuit, } from './card';
+import { Card,Compare, } from 'bee52';
+const { adjRanks,hasRank, rank, } = Card;
 
-const { rankVal, } = Rank;
-const { rank, } = Card;
-
-export const diffSuit = c0 => c1 => !sameSuit(c0)(c1);
-export const isMatch = c0 => c1 => [ sameRank(c0), sameSuit(c0), ].every(f => f(c1));
-export const xMatch = c0 => c1 => !(isMatch(c0)(c1));
-
-export const rankDiff = a => b => rankVal(rank(a)) - rankVal(rank(b));
-export const rankOrder = (a, b) => rankDiff(a)(b);
-export const rankSort = cards => cards.sort(rankOrder);
+const { sameRank, diffSuit, sameSuit, } = Compare;
 
 export const rankAdj = c0 => c1 => adjRanks(c0).map(hasRank).some(f => f(c1));
 export const suitAdj = c0 => c1 => [ diffSuit(c0), sameRank(c0), ].every(f => f(c1));
-
 export const isAdj = c0 => c1 => [ sameSuit(c0), rankAdj(c0), ].every(f => f(c1));
