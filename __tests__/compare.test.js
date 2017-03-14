8e871cff50cdcc53a660ac7c395e63b00114ee5d
theory friction practice <jackson.brebnor@gmail.com>
2017-03-13 21:05:59 -0400

    removes isAdj

--- __tests__/compare.test.js
+++ __tests__/compare.test.js
@@ -1,27 +1,24 @@
 import 'jasmine-expect';
 import { Card, } from 'bee52';
+import { rankAdj, suitAdj, } from 'src/compare';
 
 const { card, } = Card;
-
-import { rankAdj, suitAdj, } from 'src/compare';
 
 const d2 = card('2','DIAMONDS');
 const d3 = card('3','DIAMONDS');
 const c2 = card('2','CLUBS');
 const cA = card('a','CLUBS');
 
-const myCards = [ d2,d3,c2,cA, ];
-
 describe('compare', () => {
   describe('rankAdj', () => {
-    it('checks if two cards have a rankDiff of 1', () => {
+    it('checks if two cards have the same suit and adjacent ranks', () => {
       expect(rankAdj(d2)(d3)).toBeTrue();
-      expect(rankAdj(c2)(d3)).toBeTrue();
+      expect(rankAdj(c2)(d3)).toBeFalse();
       expect(rankAdj(c2)(c2)).toBeFalse();
     });
   });
   describe('suitAdj', () => {
-    it('checks if two cards are off the same suit of adjacent rank', () => {
+    it('checks if two cards are have the smae rank and different suits', () => {
       expect(suitAdj(d2)(c2)).toBeTrue();
       expect(suitAdj(c2)(d3)).toBeFalse();
       expect(suitAdj(c2)(c2)).toBeFalse();
--- src/compare.js
+++ src/compare.js
@@ -1,10 +1,5 @@
-import { Card, diffSuit, hasAdjRank, sameRank, sameSuit, } from 'bee52';
-
+import { Card, diffSuit, isRankAdj, sameRank, sameSuit, } from 'bee52';
 const { adjRanks,hasRank, } = Card;
 
-export const rankAdj = c0 => c1 => adjRanks(c0).map(hasRank).some(f => f(c1));
-
-// export const rankEq = c0 => c1 => [ sameRank(c0), diffSuit(c0), ].every(f => f(c1));
 export const suitAdj = c0 => c1 => [ sameRank(c0), diffSuit(c0), ].every(f => f(c1));
-
-export const isAdj = c0 => c1 => [ sameSuit(c0), hasAdjRank(c0), ].every(f => f(c1));
+export const rankAdj = c0 => c1 => [ sameSuit(c0), isRankAdj(c0), ].every(f => f(c1));
