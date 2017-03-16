c7fa722902f1dfc5f93aa9a577890fae001a6a5b
theory friction practice <jackson.brebnor@gmail.com>
2017-03-15 23:08:22 -0400

    player refactor scrap

--- src/player/operations.js
+++ src/player/operations.js
@@ -12,12 +12,8 @@ export const draw = amt => deck => p => add(...Deck.draw(amt)(deck))(p);
 
 export const drawTo = c => deck => p => add(...Deck.drawTo(c)(deck))(p);
 
-// export const discard = (...cards) => p => scrap(...cards)(p);
-
 export const play = cards => p => playable(cards) ? 
   setPlays([ ...plays(p),cards, ])(scrap(...cards)(p)) : p;
 
 export const score = p => total(plays(p));
 export const final = p => total(plays(p)) + deduct(hand(p));
-
-// export const isSequene
