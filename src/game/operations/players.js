5019f41a5a0cf63710caf95d698a671539cdf720
theory friction practice <jackson.brebnor@gmail.com>
2017-04-04 12:36:07 -0400

    operations

--- src/game/operations/players.js
+++ src/game/operations/players.js
@@ -1,8 +1,9 @@
 import { filter, map, } from 'fenugreek-collections';
 import { addHand, matches, scrap, update, xMatches, } from '../../player';
-import { active, next, players, setPlayers as setP, } from '../data';
+import { active, discard, next, players, setPlayers as setP, } from '../data';
 import { disDel, drop, } from './discard';
 import { shiftDk, } from './deck';
+import { Deck, } from 'bee52';
 const { add, drop: dropD, drawTo: upTo, } = Deck;
 
 export const rotate = ([ first, ...rem ]) => [ ...rem, first, ];
@@ -14,21 +15,21 @@ export const pushPlr = p => g => setP(players(g).concat(p))(g);
 export const addPlr = p => g => hasPlr(p)(g) ? mendPlr(p)(g) : pushPlr(p)(g);
 export const rmPlr = p => g => setP(filter(players(g))(xMatches(p)))(g);
 
+export const isActive = g => p => matches(active(g))(p);
+
 export const claimCards = (...cards) => p => g =>
   addPlr(addHand(...cards)(p))(g); 
 
 export const dropCards = (...cards) => p => g =>
   addPlr(scrap(...cards)(p))(drop(...cards)(g)); 
-  
-export const isActive = g => p => matches(active(g))(p);
 
 export const deckDraw = p => g => 
-    isActive(g)(p) ? claimCards(next(g))(p)(shiftDk(g)) : g;
-    
-export const disDraw = p => g => 
-    isActive(g)(p) ? claimCards(next(g))(p)(shiftDk(g)) : g;
-    
-export const drawTo = card => g => 
-  disDraw(...upTo(card)(discard(g)))(g);
+  isActive(g)(p) ? claimCards(next(g))(p)(shiftDk(g)) : g;
+
+export const disDraw = (...cards) => p => g => 
+  isActive(g)(p) ? claimCards(...cards)(p)(disDel(...cards)(g)) : g;
+
+export const drawTo = c => p => g => 
+  isActive(g)(p) ? disDraw(...upTo(c)(discard(g)))(p)(g) : g;
 
 export const claim = (...cards) => g => claimCards(...cards)(active(g))(g);
