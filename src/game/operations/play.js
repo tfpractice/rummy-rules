9a51fc57c33c7e4bd921bb77bed03a157fd51f06
theory friction practice <jackson.brebnor@gmail.com>
2017-04-13 13:36:38 -0400

    playus and playables

--- src/game/operations/play.js
+++ src/game/operations/play.js
@@ -7,12 +7,11 @@ import { deckDel, } from './deck';
 import { disDel, } from './discard';
 import { hasMatch, } from '../../deck';
 
-export const claimSet = set => p => g => addPlr(addSets(set)(p))(g);
+export const claimSet = set => p => g => addPlr(addSets(set)(p))(clearCards(...set)(g));
 export const playable = g => set => canPlay(allSets(g))(set);
 export const clearCards = (...set) => g => deckDel(...set)(disDel(...set)(g));
 
-export const play = set => p => g =>
- canPlay(allSets(g))(set) ? claimSet(set)(p)(deckDel(...set)(g)) : g;
+export const play = set => p => g => canPlay(allSets(g))(set) ? claimSet(set)(p)(g) : g;
 
 export const rumCheck = g => plays(allSets(g))(discard(g)).length > 0;
 export const rummable = g => plays(allSets(g))(discard(g));
